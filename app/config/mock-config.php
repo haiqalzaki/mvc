b3213254495a40d6da9a982cdd70506243a2ce32
Haiqal <haiqalzaki.hz@gmail.com>
2024-12-23 17:52:49 +0800

    added mock config

---
+++ app/config/mock-config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL','http://localhost/your/path');
+
+// DB
+define('DB_HOST','yourIP');
+define('DB_USER','yourUsername');
+define('DB_PASS','yourPassword');
+define('DB_NAME','YourDatabase');
